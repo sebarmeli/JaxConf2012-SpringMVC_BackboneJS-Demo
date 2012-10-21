521fef148b3675049b1ce22e11250cf410e20229
Sebastiano Armeli-Battana <seba.armeli@gmail.com>
2012-10-21 23:57:02 +1200

    Update src/main/resources/sql/contact.sql

--- src/main/resources/sql/contact.sql
+++ src/main/resources/sql/contact.sql
@@ -2,6 +2,7 @@ CREATE TABLE `Contacts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(30) DEFAULT NULL,
   `lastname` varchar(30) DEFAULT NULL,
-  `telephone` varchar(15) DEFAULT NULL
+  `telephone` varchar(15) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
